f43d1562f9566ef2bbec9425dbb36e85c073f316
omkarg01 <omkargujja01@gmail.com>
2022-11-05 21:27:58 +0530

    enum done

---
+++ contracts/Enum.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Enum {
+    enum Status {
+        Pending,
+        Shipped,
+        Accepted,
+        Rejected,
+        Canceled
+    }
+
+    Status public status;
+
+    function set(Status _status) public {
+        status = _status;
+    }
+
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    function cancel() public {
+        status = Status.Accepted;
+    }
+}
