1fdec41a5d23d89a0a604aefcc6987b89154c27f
omkarg01 <omkargujja01@gmail.com>
2022-11-13 22:54:21 +0530

    Inheritance done

---
+++ contracts/Inheritance.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract A {
+    function foo() public pure virtual returns (string memory) {
+        return "A";
+    }
+}
+
+contract B is A {
+    function foo() public pure virtual override returns (string memory) {
+        return "B";
+    }
+}
+
+contract C is A {
+    function foo() public pure virtual override returns (string memory) {
+        return "C";
+    }
+}
+
+contract D is B, C {
+    function foo() public pure virtual override(B, C) returns (string memory) {
+        return super.foo();
+    }
+}
+
+
